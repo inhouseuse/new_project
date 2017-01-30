2ca458c9e54c078dbe0da11a3a1a7179d82aed18
nokogiring <beefmeup@gmail.com>
2017-01-30 17:02:08 +0900

    sample end

---
+++ dist/scripts/main.js
@@ -0,0 +1,12 @@
+function hello(compiler) {
+    console.log("Hello from " + compiler);
+}
+hello("TypeScript");
+hello('aaa');
+$(function () {
+    $("#sample-calender").datepicker();
+    $('#title').click(function () {
+        console.log("aaa");
+        alert(this.target.tagName);
+    });
+});
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,8 +2,14 @@ function hello(compiler: string) {
     console.log(`Hello from ${compiler}`);
 }
 hello("TypeScript");
+hello('aaa');
 
 
 $(function() {
-  $( "#sample-calender" ).datepicker();
+  $("#sample-calender").datepicker();
+  $('#title').click(function(){
+    console.log("aaa");
+    alert(this.target.tagName);
+  });
 });
+
