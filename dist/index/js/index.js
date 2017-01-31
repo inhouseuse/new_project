e3cc2e41dca59e485fdb105e3af644ec8710b9d1
nokogiring <beefmeup@gmail.com>
2017-01-31 12:56:08 +0900

    indexの追加

---
+++ dist/index/js/index.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#app-index',
+    data: {
+        msg: 'aaa',
+        pages:[
+            {
+                name: 'p1',
+                url: 'http://materializecss.com/getting-started.html',
+                imageUrl: ''
+            },
+            {
+                name: 'p2',
+                url: 'http://materializecss.com/getting-started.html',
+                imageUrl: ''
+            },
+        ]
+    }
+})
