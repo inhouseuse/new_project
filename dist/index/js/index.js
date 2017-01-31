ce1a3b9f86f7dc6cb263fcda05252f15af767a4c
nokogiring <beefupme@gmail.com>
2017-01-31 22:11:47 +0900

    index修正中

--- dist/index/js/index.js
+++ dist/index/js/index.js
@@ -5,14 +5,21 @@ new Vue({
         pages:[
             {
                 name: 'p1',
-                url: 'http://materializecss.com/getting-started.html',
+                url: '.././sample.html',
                 imageUrl: ''
             },
             {
                 name: 'p2',
-                url: 'http://materializecss.com/getting-started.html',
+                url: '.././sample.html',
                 imageUrl: ''
             },
         ]
+    },
+    methods: {
+        openWindow: function(page) {
+            var win;
+            var features = "width='1024px',menubar=no,location=no,resizable=yes,scrollbars=yes,status=no";
+            window.open(page.url, page.name, features);
+        }
     }
 })
